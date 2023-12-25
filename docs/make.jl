979d912eef2329bc2843283276d3ad48bbb5acab
Avik Pal <avikpal@mit.edu>
2023-12-25 17:30:48 -0500

    Run CI correctly

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,17 @@
 using PolyesterForwardDiff
 using Documenter
 
-DocMeta.setdocmeta!(Polyester, :DocTestSetup, :(using PolyesterForwardDiff); recursive=true)
+DocMeta.setdocmeta!(PolyesterForwardDiff, :DocTestSetup,
+    :(using PolyesterForwardDiff); recursive=true)
 
 makedocs(;
     modules=[PolyesterForwardDiff],
     authors="Chris Elrod <elrodc@gmail.com> and contributors",
-    repo="https://github.com/JuliaSIMD/PolyesterForwardDiff.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/JuliaDiff/PolyesterForwardDiff.jl/blob/{commit}{path}#{line}",
     sitename="PolyesterForwardDiff.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://JuliaSIMD.github.io/PolyesterForwardDiff.jl",
+        canonical="https://juliadiff.org/PolyesterForwardDiff.jl",
         assets=String[],
     ),
     pages=[
@@ -19,5 +20,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/JuliaSIMD/PolyesterForwardDiff.jl",
+    repo="github.com/JuliaDiff/PolyesterForwardDiff.jl",
 )
